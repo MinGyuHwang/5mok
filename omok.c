8272119eae58ec610849193dce083ca6a6d9fc79
MinGyuHwang <pajuhmg@naver.com>
2023-07-19 19:41:04 +0900

    0719

--- omok.c
+++ omok.c
@@ -1,65 +1,98 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
 #define GRID_SIZE 19
 
-
-
 char Grid_arr[GRID_SIZE][GRID_SIZE]; // 전역변수로 선언
+int row, col = 0;
 
 void initGrid()
 {
-	for (int i = 0; i < GRID_SIZE; i++) 
-	{
-		for (int j = 0; j < GRID_SIZE; j++)
-		{
-			Grid_arr[i][j] = '+'; // 초기값 '┼'로 설정
-		}
-	}
-
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            Grid_arr[i][j] = '+';
+        }
+    }
 }
+
 void printGrid()
 {
-	for (int i = 0; i < GRID_SIZE; i++)
-	{
-		for (int j = 0; j < GRID_SIZE; j++) 
-		{
-			printf("%c ", Grid_arr[i][j]);
-		}
-		printf("\n");
-	}
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            printf("%c ", Grid_arr[i][j]);
+        }
+        printf("\n");
+    }
 }
-void replaceStone(int row, int col)
+
+void replacestone1()
 {
-	if ((Grid_arr [row][col] == '+') && ~(Grid_arr [row][col] == '○'))
-	{
-		Grid_arr [row][col] = '● ';
-	}
-	else if ((Grid_arr [row][col] == '+') && ~(Grid_arr [row][col] == '●'))
-	{
-		Grid_arr [row][col] = '○ ';
-	}
-	else
-	{
-		printf("오류 입니다.\n");
-	}
+    printf("행 번호를 입력하세요 : ");
+    scanf("%d", &row);
+    printf("\n열 번호를 입력하세요 : ");
+    scanf("%d", &col);
+    if ((row >= 1) && (col >= 1) && (row <= GRID_SIZE) && (col <= GRID_SIZE))
+    {
+        if ((Grid_arr[row][col] == '+') && (Grid_arr[row][col] != 'O'))
+        {
+            Grid_arr[row][col] = 'X';
+        }
+        else
+        {
+            printf("오류 입니다.\n");
+        }
+    }
+}
+
+void replacestone2()
+{
+    printf("행 번호를 입력하세요 : ");
+    scanf("%d", &row);
+    printf("\n열 번호를 입력하세요 : ");
+    scanf("%d", &col);
+    if ((row >= 1) && (col >= 1) && (row <= GRID_SIZE) && (col <= GRID_SIZE))
+    {
+        if ((Grid_arr[row][col] == '+') && (Grid_arr[row][col] != 'X'))
+        {
+            Grid_arr[row][col] = 'O';
+        }
+        else
+        {
+            printf("오류 입니다.\n");
+        }
+    }
 }
 
 int main()
 {
-	initGrid();
-	int row, col=0;
-	while (1)
-	{
-		printGrid();
-		printf("행 번호를 입력하세요 : ");
-		scanf("%d", &row);
-		printf("\n열 번호를 입력하세요 : ");
-		scanf("%d", &col);
+    initGrid(); // grid 초기화작업
 
-		if ((row >= 1) && (col >= 1) && (row <= GRID_SIZE) && (col <= GRID_SIZE))
-			replaceStone(row - 1, col - 1);
-		else
-			printf("잘못된 입력입니다.");
-	}
+    int player = 1; // 플레이어 번갈아가며 돌을 놓을 차례
+
+    while (1)
+    {
+        printGrid();
+        if (player == 1)
+        {
+            printf("플레이어 1의 차례 (돌: X)\n");
+            replacestone1();
+            player = 2;
+        }
+        else
+        {
+            printf("플레이어 2의 차례 (돌: O)\n");
+            replacestone2();
+            player = 1;
+        }
+
+        // 게임 종료 조건 추가 (추가할 조건 필요)
+    }
+
+    return 0;
 }
