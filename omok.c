abc81f651fbdbfcf65ef8b743fe17a7ae17dc5f2
MinGyuHwang <pajuhmg@naver.com>
2023-07-19 18:52:18 +0900

    0719

---
+++ omok.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define GRID 19
+
+void drawing_board()
+{
+	for (int j = 0; j < GRID; j++)
+	{
+		for (int i = 0; i < GRID; i++)
+		{
+			printf(" ┼");
+		}
+		printf("\n");
+	}
+} //19x19 격자 만드는 함수
+
+void put_black()
+{
+
+}
+
+void put_white()
+void(compare_rocks)
+int main()
+{
+	drawing_board();
+}
